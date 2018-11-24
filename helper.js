f4d3453aae2963ca351566d0ed786e77bf191e1f
Alexis Tan <alexis@sen-dev.com>
2018-11-24 18:17:40 +0100

    Refactor + add some documentation

---
+++ helper.js
@@ -0,0 +1,26 @@
+
+/**
+ * Formats the list so it will look nice when converted to markdown.
+ * 
+ * @param  {Array}  array
+ * @param  {Object} command
+ * @return {Boolean|String}
+ */
+module.exports.formatList = function (array, command) {
+    if (array == null) {
+        return false;
+    }
+    return array.map(line => {
+        let parts = line.split('-');
+        if (parts.length <= 1) {
+            return format(line, command);
+        }
+        return format(parts[0], command) + `\n\t- ${parts[1]}`;
+    }).join('\n');
+};
+
+function format(line, command) {
+    return '\t' + line.replace(/\:command/, command.trigger)
+        .replace(/`/g, '')
+        .trim();
+}
--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { formatList } = require('./helper.js');
 const Mustache = require('mustache');
 const fs = require('fs');
 
@@ -14,21 +15,33 @@ const templates = {
     commandFull: fs.readFileSync(`./templates/_command-full.md`, { encoding: 'utf8' })
 }
 
+// Sets up the the commands and categories variables, each variable will eventually hold
+// all the formatted command or category data, the command map wil be sorted into
+// "short" and "full" types for generating different types of command outputs.
 const commands = {};
 const categories = [];
 for (let categoryName in commandMap) {
     statistics.categories++;
 
+    // Creates the command "short" and "full" arrays that
+    // should be stored in the commands map.
     let categoryCommandsFull = [];
     let categoryCommandsShort = [];
+
+    // Stores the default prefix for the command category, this
+    // will be used later when creating command triggers.
     let categoryPrefix = commandMap[categoryName].prefix;
     for (let commandName in commandMap[categoryName].commands) {
         statistics.commands++;
 
+        // Creates the command variable used for the templates, and
+        // sets up some helpful keys for specific outputs.
         let command = commandMap[categoryName].commands[commandName];
         command.trigger = categoryPrefix + command.triggers[0];
         command.shortDescription = command.description.split('\n')[0];
 
+        // Creates the command relationships, setting them to false by default, otherwise
+        // storing them as a list of objects with the "name" and "command" keys.
         let commandRelationships = false;
         if (command.relationships != null) {
             commandRelationships = [];
@@ -43,15 +56,12 @@ for (let categoryName in commandMap) {
             }
         }
 
+        // Stores the command in its short and full versions.
         categoryCommandsShort.push({ command });
         categoryCommandsFull.push({
             command: Mustache.render(templates.commandFull, {
-                commandUsage: command.usage === null ? false : command.usage.map(line => {
-                    return '\t' + line.replace(/\:command/, command.trigger).replace(/`/g, '');
-                }).join('\n'),
-                commandExample: command.example === null ? false : command.example.map(line => {
-                    return '\t' + line.replace(/\:command/, command.trigger).replace(/`/g, '');
-                }).join('\n'),
+                commandUsage: formatList(command.usage, command),
+                commandExample: formatList(command.example, command),
                 commandRelationships,
                 commandName,
                 command,
@@ -59,12 +69,16 @@ for (let categoryName in commandMap) {
         });
     }
 
+    // Stors the "short" and "full" version of the commands under the current category.
     commands[categoryName.toLowerCase()] = {
         short: categoryCommandsShort,
         full: categoryCommandsFull,
     };
 }
 
+/**
+ * Generates the output based of the created data and the templates.
+ */
 fs.readdir('./templates', (err, files) => {
     for (let file of files) {
         if (file.substr(0, 1) == '_') {
@@ -73,6 +87,8 @@ fs.readdir('./templates', (err, files) => {
 
         console.log(`Formatting ${file}`);
         
+        // Writes the generated ouput to a file with the same name as
+        // the template, storing it in the "output" directory.
         fs.writeFile(
             `./output/${file}`, 
             Mustache.render(
