71c87eb438dcc1b9e2ff231aa541625deaa1fec9
Alexis Tan <alexis@sen-dev.com>
2018-11-24 16:10:48 +0100

    Make templates work using mustache

--- index.js
+++ index.js
@@ -1,2 +1,38 @@
+const Mustache = require('mustache');
+const fs = require('fs');
 
-console.log('Hello, World!');
+const commands = JSON.parse(
+    fs.readFileSync('commandMap.json', { encoding: 'utf8' })
+);
+
+const statistics = {
+    categories: 0,
+    commands: 0
+};
+
+for (let categoryName in commands) {
+    statistics.categories++;
+    for (let commandName in commands[categoryName].commands) {
+        statistics.commands++;
+    }
+}
+
+fs.readdir('./templates', (err, files) => {
+    for (let file of files) {
+        console.log(`Formatting ${file}`);
+        
+        fs.writeFile(
+            `./output/${file}`, 
+            Mustache.render(
+                fs.readFileSync(`./templates/${file}`, { encoding: 'utf8' }), {
+                    statistics, commands
+                }
+            ),
+            err => {
+                if (err) {
+                    return console.error(`Failed to save ${file}`, err);
+                }
+            }
+        );
+    }
+});
---
+++ templates/command-list.md
@@ -0,0 +1,9 @@
+# Command List
+
+Below you'll find a list of all **{{ statistics.commands }}** commands that Ava has, along with a short description of what each command does. If you'd like to know more about the command, like what permissions or roles are required to run the command, different aliases or anything similar you can click on the command links and you'll be taken to a more descriptive version of the command, or use the help command for the given command in a Discord server.
+
+> All commands can be used by mentioning Ava first, followed by the command you want to run, for example `@AvaIre ping` will run the ping command, or `@AvaIre poke @Senither` will run the poke command for Senither.
+
+## Table of Contents
+
+ -- Dynamically generate the command list here --
